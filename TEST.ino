760b1d44858e7bdf2559ed6a25f53f69ee936c38
yeisonro <yeisonro@gmail.com>
2016-08-16 13:01:10 -0500

    Add files via upload

---
+++ TEST.ino
@@ -0,0 +1,40 @@
+
+
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+
+LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+char HABITACIONN;
+int llamadohabitacion = 0;
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+    // Print a message to the LCD.
+   
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("T");
+  lcd.setCursor(1,0);
+  lcd.print("E"); 
+  lcd.setCursor(2,0);
+  lcd.print("S"); 
+  lcd.setCursor(3,0);
+  lcd.print("T"); 
+  lcd.setCursor(0,1);
+  lcd.print("L");  
+  lcd.setCursor(1,1);
+  lcd.print("C"); 
+   lcd.setCursor(2,1);
+  lcd.print("D");  
+  
+}
+
+
+void loop()
+{
+}
